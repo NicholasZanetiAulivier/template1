05185675d019f9aaae894744696b09c2a86fc37a
NicholasZanetiAulivier <nicholas.535240054@stu.untar.id>
2025-03-26 13:06:53 +0700

    final fix

--- src/api/components/authentication/authentication-controller.js
+++ src/api/components/authentication/authentication-controller.js
@@ -1,31 +1,34 @@
 const authService = require('./authentication-service');
 const { errorResponder, errorTypes } = require('../../../core/errors');
 
-async function postLogin(request, response) {
+async function postLogin(request, response, next) {
   const { email, password } = request.body;
+  try {
+    if (!email) {
+      throw errorResponder(errorTypes.VALIDATION_ERROR, 'Email is required');
+    }
+    if (!password) {
+      throw errorResponder(errorTypes.VALIDATION_ERROR, 'Password is required');
+    }
 
-  if (!email) {
-    throw errorResponder(errorTypes.VALIDATION_ERROR, 'Email is required');
+    const emailExists = await authService.emailExists(email);
+    if (!emailExists) {
+      throw errorResponder(errorTypes.INVALID, 'No such user with email');
+    }
+
+    const validCredentials = await authService.validate(email, password);
+
+    if (!validCredentials) {
+      throw errorResponder(
+        errorTypes.INVALID_PASSWORD,
+        'Registered password does not match'
+      );
+    }
+
+    return response.status(200).json(await authService.getUserByEmail(email));
+  } catch (err) {
+    return next(err);
   }
-  if (!password) {
-    throw errorResponder(errorTypes.VALIDATION_ERROR, 'Password is required');
-  }
-
-  const emailExists = await authService.emailExists(email);
-  if (!emailExists) {
-    throw errorResponder(errorTypes.INVALID, 'No such user with email');
-  }
-
-  const validCredentials = await authService.validate(email, password);
-
-  if (!validCredentials) {
-    throw errorResponder(
-      errorTypes.INVALID_PASSWORD,
-      'Registered password does not match'
-    );
-  }
-
-  return response.status(200).json(await authService.getUserByEmail(email));
 }
 
 module.exports = {
